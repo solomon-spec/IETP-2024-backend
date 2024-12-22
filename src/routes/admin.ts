488d9878648d08a25ce5c445c8c478c61e82495b
solomon Abate <solomonabate18@gmail.com>
2024-12-22 17:42:20 +0300

    Admin functionality

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,9 +1,26 @@
 import express from 'express';
-import { registerUser } from '../controllers/userController';
+import { registerUser, getUsers, updateUser } from '../controllers/userController';
+import { createClass, getClasses, updateClass, deleteClass } from '../controllers/classController';
+import { createCourse, getCourses, updateCourse, deleteCourse } from '../controllers/courseController';
 import { isAdmin } from '../middleware/authMiddleware';
 
 const router = express.Router();
-
+// User routes
 router.post('/register', isAdmin, registerUser);
+router.get('/users', isAdmin, getUsers);
+router.put('/users/:id', isAdmin, updateUser);
+
+// Class routes
+router.post('/classes', isAdmin, createClass);
+router.get('/classes', isAdmin, getClasses);
+router.put('/classes/:id', isAdmin, updateClass);
+router.delete('/classes/:id', isAdmin, deleteClass);
+
+// Course routes
+router.post('/courses', isAdmin, createCourse);
+router.get('/courses', isAdmin, getCourses);
+router.put('/courses/:id', isAdmin, updateCourse);
+router.delete('/courses/:id', isAdmin, deleteCourse);
+
 
 export default router;
