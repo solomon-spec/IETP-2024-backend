fb2464508786db47b8ebbc49e125b837b1a29d05
solomon Abate <solomonabate18@gmail.com>
2024-12-22 11:39:35 +0300

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+// src/app.ts
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+export default app;
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+// src/index.ts
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import app from './app';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI as string;
+
+mongoose.connect(MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(err => {
+    console.error('Failed to connect to MongoDB', err);
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
